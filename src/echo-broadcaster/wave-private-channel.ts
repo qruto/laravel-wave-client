4a72d7db7c6165a5a0627d2dd066db1c49008648
Slava Razum <modern.web.artisan@gmail.com>
2022-08-01 21:25:50 +0300

    Corrected scalar types receiving on whisper events

--- src/echo-broadcaster/wave-private-channel.ts
+++ src/echo-broadcaster/wave-private-channel.ts
@@ -2,11 +2,14 @@ import request from '../util/request';
 import { AuthRequest, authRequest } from '../channel-auth';
 
 import WaveChannel from './wave-channel';
+import { Channel } from 'laravel-echo';
 
 export default class WavePrivateChannel extends WaveChannel {
     protected authorized = false;
 
     protected afterAuthCallbacks: Record<string, (() => void)[]> = {};
+
+    protected whisperCallbacks = new Map<Function, Function>();
 
     protected auth: AuthRequest;
 
@@ -18,6 +21,29 @@ export default class WavePrivateChannel extends WaveChannel {
 
     public whisper(eventName, data) {
         request(this.connection).post(this.options.endpoint + '/whisper', { channel_name: this.name, event_name: eventName, data });
+
+        return this;
+    }
+
+    public listenForWhisper(event: string, callback: Function): WavePrivateChannel {
+        let listener = function (data) {
+            callback(Array.isArray(data) && data.length === 1 && typeof data[0] !== 'object' ? data[0] : data);
+        };
+
+        this.whisperCallbacks.set(callback, listener);
+
+        super.listenForWhisper(event, listener);
+
+        return this;
+    }
+
+    public stopListeningForWhisper(event: string, callback?: Function): WavePrivateChannel {
+        if (callback) {
+            callback = this.whisperCallbacks.get(callback);
+            this.whisperCallbacks.delete(callback);
+        }
+
+        super.stopListeningForWhisper(event, callback);
 
         return this;
     }

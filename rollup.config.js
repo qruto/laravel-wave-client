262b7d91411703acba810407f55d79bf2c509ce4
Slava Razum <modern.web.artisan@gmail.com>
2022-08-02 20:06:17 +0300

    Fixed iife build

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,38 @@
 import babel from '@rollup/plugin-babel';
-import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 
-export default {
-    input: './src/wave.ts',
-    output: [
-        { file: './dist/wave.js', format: 'esm' },
-        { file: './dist/wave.common.js', format: 'cjs' },
-        { file: './dist/wave.iife.js', format: 'iife', name: 'Wave' },
-    ],
-    plugins: [
-        resolve(),
-        typescript(),
-        babel({
-            babelHelpers: 'bundled',
-            exclude: 'node_modules/**',
-            extensions: ['.ts'],
-            presets: ['@babel/preset-env'],
-            plugins: [
-                ['@babel/plugin-proposal-decorators', { legacy: true }],
-                '@babel/plugin-proposal-function-sent',
-                '@babel/plugin-proposal-export-namespace-from',
-                '@babel/plugin-proposal-numeric-separator',
-                '@babel/plugin-proposal-throw-expressions',
-                '@babel/plugin-transform-object-assign',
-            ],
-        }),
-    ],
-    external: ['laravel-echo'],
-};
+const plugins = [
+    typescript(),
+    babel({
+        babelHelpers: 'bundled',
+        exclude: 'node_modules/**',
+        extensions: ['.ts'],
+        presets: ['@babel/preset-env'],
+        plugins: [
+            ['@babel/plugin-proposal-decorators', { legacy: true }],
+            '@babel/plugin-proposal-function-sent',
+            '@babel/plugin-proposal-export-namespace-from',
+            '@babel/plugin-proposal-numeric-separator',
+            '@babel/plugin-proposal-throw-expressions',
+            '@babel/plugin-transform-object-assign',
+        ],
+    }),
+];
+
+export default [
+    {
+        input: './src/wave.ts',
+        output: [
+            { file: './dist/wave.js', format: 'esm' },
+            { file: './dist/wave.common.js', format: 'cjs' },
+        ],
+        plugins,
+        external: ['laravel-echo'],
+    },
+    {
+        input: './src/index.iife.ts',
+        output: [{ file: './dist/wave.iife.js', format: 'iife', name: 'Wave' }],
+        plugins,
+        external: ['laravel-echo'],
+    },
+];
--- src/wave.ts
+++ src/wave.ts
@@ -1,6 +1,8 @@
 import { Wave } from "./wave-broadcaster";
 import { WaveConnector } from "./echo-broadcaster/wave-connector";
 
+export default Wave;
+
 export {
     Wave,
     WaveConnector,

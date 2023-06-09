12a34a09d5b08d1aa9867dafed9a3cf246cbbeeb
Slava Razum <razum@qruto.to>
2023-06-09 13:29:03 +0200

    Debug `false` by default

--- src/EventSourceConnection.ts
+++ src/EventSourceConnection.ts
@@ -24,7 +24,7 @@ export class EventSourceConnection {
         options = {
             reconnect: true,
             pauseInactive: false,
-            debug: true,
+            debug: false,
             ...options,
         }
 

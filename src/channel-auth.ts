bf0ee1e9fe7f37238c9096773380a182a2d5ebd0
Slava Razum <razum@qruto.to>
2023-07-20 22:22:59 +0200

    Refactor to promise usage

--- src/channel-auth.ts
+++ src/channel-auth.ts
@@ -3,11 +3,6 @@ import request from "./util/request";
 import { EventSourceConnection } from "./EventSourceConnection";
 import { Options } from './echo-broadcaster/wave-connector';
 
-export interface AuthRequest {
-    after: (callback: Function) => AuthRequest;
-    response: Promise<void>;
-}
-
 export class AuthRequestError extends Error {
     public response: Response;
     constructor(message: string, response: Response) {
@@ -17,38 +12,14 @@ export class AuthRequestError extends Error {
     }
 }
 
-export function authRequest(channel: string, connection: EventSourceConnection, options: Options): AuthRequest {
-    let authorized = false;
-    let afterAuthCallbacks: Function[] = [];
-
-    function after(callback: Function) {
-        if (authorized) {
-            callback();
-
-            return;
-        }
-
-        afterAuthCallbacks.push(callback);
-
-        return this;
-    }
-
-    const response = request(connection)
+export function authRequest(channel: string, connection: EventSourceConnection, options: Options) {
+    return request(connection)
         .post(options.authEndpoint, options, { channel_name: channel })
         .then((response) => {
             if (!response.ok) {
                 throw new AuthRequestError(`Auth request to failed with status ${response.status}`, response);
             }
 
-            authorized = true;
-
-            afterAuthCallbacks.forEach((callback) => callback(response));
-
-            afterAuthCallbacks = [];
+            return response;
         });
-
-    return {
-        after,
-        response,
-    }
 }
